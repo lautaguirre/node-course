b83ee0cf00863cacc203bde2867693eb9c769dd9
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-04-28 22:30:18 -0300

    weahter-app update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,58 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
+
+// Define paths and port
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 const port = 3001;
 
+// Template engine set up
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Static directory set up
+app.use(express.static(publicDirectoryPath));
+
 app.get('', (req, res) => {
-  res.send('Holis');
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Lautaro',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
+  res.render('about', {
+    title: 'About Me',
+    name: 'Lautaro',
+  });
 });
 
-app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'Caen nieve',
-    location: 'Rosario',
- });
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Lautaro',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    error: 'Help article not found',
+    title: '404',
+    name: 'Lautaro',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    error: 'Page not found',
+    title: '404',
+    name: 'Lautaro',
+  });
 });
 
 app.listen(port, () => {
