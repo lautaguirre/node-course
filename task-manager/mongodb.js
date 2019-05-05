6c4a58eb3302c9782b5940f87ab9b1c10883b976
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-05-05 12:58:06 -0300

    created task-manager project

---
+++ task-manager/mongodb.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect');
+  }
+
+  const db = client.db(databaseName);
+
+  db.collection('users').insertOne({
+    name: 'Lautaro',
+    age: '24',
+  });
+});
