6ce46b3e15b6e20bc4923efb82ee71769f695906
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-05-26 17:10:06 -0300

    added users test ; added task tests ; fixtures ; finished tasks app

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,24 +1,9 @@
 const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
+const { userOne, userOneId, setupDatabase } = require('./fixtures/db');
 
-const userOneId  = new mongoose.Types.ObjectId();
-const userOne = {
-  _id: userOneId,
-  name: 'Jest1',
-  email: 'jest@tester.com',
-  password: 'test12345',
-  tokens: [{
-    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-  }],
-};
-
-beforeEach(async () => {
-  await User.deleteMany();
-  await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test('Should sign up a new user', async () => {
   const response = await request(app)
@@ -99,4 +84,39 @@ test('Should not delete user\'s account', async () => {
     .delete('/users/me')
     .send()
     .expect(401);
+});
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user).not.toBeNull();
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Carlos',
+    })
+    .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe('Carlos');
+});
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Buenos Aires',
+    })
+    .expect(400);
 });
