838ad5bd7aaf1dfd4af993531ac365ac1cee0447
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-05-29 22:33:07 -0300

    acknowledgements

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,9 +1,48 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log(count);
+// Elements
+const $messageForm = document.querySelector('#messageForm');
+const $messafeFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $locationButton = document.querySelector('#sendLocation');
+
+socket.on('message', (message) => {
+  console.log(message);
 });
 
-const button = document.querySelector('#increment').addEventListener('click', () => {
-  socket.emit('increment');
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messafeFormInput.value = '';
+    $messafeFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message Delivered');
+  });
+});
+
+$locationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  $locationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+
+    socket.emit('sendLocation', { latitude, longitude }, () => {
+      console.log('Location Delivered');
+      $locationButton.removeAttribute('disabled');
+    });
+  });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,17 +14,30 @@ const publicDirectory = path.join(__dirname, '../public');
 app.use(express.json());
 app.use(express.static(publicDirectory));
 
-let count = 0;
-
 io.on('connection', (socket) => {
   console.log('Web socket connection');
 
-  socket.emit('countUpdated', count);
+  socket.emit('message', 'Welcome');
+  socket.broadcast.emit('message', 'A new user has joined');
 
-  socket.on('increment', () => {
-    count++;
-    // socket.emit('countUpdated', count);
-    io.emit('countUpdated', count);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed');
+    }
+
+    io.emit('message', message);
+    callback();
+  });
+
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+    io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A client has disconnected');
   });
 });
 
